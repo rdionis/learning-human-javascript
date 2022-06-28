e77abaecdba0bda9bfd99158048862c469247b1d
Raquel Dionísio <dionisio@stadtwerk.org>
2022-06-28 18:30:07 +0200

    adapting webpack settings on my own

--- labelr/dist/main.js
+++ labelr/dist/main.js
@@ -19,14 +19,14 @@ eval("/* module decorator */ module = __webpack_require__.nmd(module);\nvar __WE
 
 /***/ }),
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst component = () => {\n    const element = document.createElement(\"div\");\n    \n    // Lodash, now imported by this script\n    element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join([\"Hello\", \"Ampersand by Raquel\"], \" \");\n\n    return element\n}\n\ndocument.body.appendChild(component())\n\n//# sourceURL=webpack://labelr/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst component = () => {\n    const element = document.createElement(\"h1\");\n    \n    // Lodash, now imported by this script\n    element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join([\"Hello\", \"Ampersand by Raquel\"], \" \");\n\n    return element\n}\n\ndocument.body.appendChild(component())\n\n//# sourceURL=webpack://labelr/./src/app.js?");
 
 /***/ })
 
@@ -126,7 +126,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var loda
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.js");
 /******/ 	
 /******/ })()
 ;
--- labelr/src/app.js
+++ labelr/src/app.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 
 const component = () => {
-    const element = document.createElement("div");
+    const element = document.createElement("h1");
     
     // Lodash, now imported by this script
     element.innerHTML = _.join(["Hello", "Ampersand by Raquel"], " ");
